946204ed350dad3d60b48bf505239d5f9a842ccb
Marumaru0000 <k-maruyama-5x3@eagle.sophia.ac.jp>
2024-11-29 17:27:06 +0900

    Clear cache and update configuration

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('../order');
+    return view('/order');
 });
-#use Illuminate\Support\Facades\Route;
 
 // セルフオーダーシステムのトップページとしてQRコードの表示ページを設定
 #Route::view('/', 'ordering::help');
