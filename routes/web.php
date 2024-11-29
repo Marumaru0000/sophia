4cb71be73a203859fe5119e358692ede5d79fce4
Marumaru0000 <k-maruyama-5x3@eagle.sophia.ac.jp>
2024-11-30 00:40:46 +0900

    Clear cache and update configuration

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 // セルフオーダーシステムのトップページの設定
 Route::redirect('/', '/order');
+
+Route::get('order/{table?}', function () {
+    return view('ordering::order.index', [
+        'table' => request('table'),
+    ]);
+})->name('order');
