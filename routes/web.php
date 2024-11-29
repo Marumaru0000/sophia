31fd488e2bffa298d855c86e7bb198c8393b7153
Marumaru0000 <k-maruyama-5x3@eagle.sophia.ac.jp>
2024-11-29 14:20:18 +0900

    Update configurations and rebuild assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+#use Illuminate\Support\Facades\Route;
+
+#Route::get('/', function () {
+    #return view('welcome');
+#});
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// セルフオーダーシステムのトップページとしてQRコードの表示ページを設定
+Route::view('/', 'ordering::help');
